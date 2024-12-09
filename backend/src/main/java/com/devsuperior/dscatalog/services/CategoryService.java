f76e4c533ec4d14167de366ab7c54ab856923ebb
joaos <joao.sousa.12145@gmail.com>
2024-12-09 10:37:59 -0300

    Criação do DTO da classe Category.

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dscatalog.services;
 
 
+import com.devsuperior.dscatalog.DTOs.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -15,8 +19,11 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
-    
-    public List<Category> findAll() {
-        return repository.findAll();
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
     }
 }
