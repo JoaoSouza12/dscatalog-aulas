e545f6a827ec4fda37d699236b816ffcf18b9afb
João <joao.sousa.12145@gmail.com>
2024-12-11 11:42:13 -0300

    fixup! Criação do método insert da entidade Categoria.

--- backend/src/main/java/com/devsuperior/dscatalog/expections/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.dscatalog.expections;
-
-public class EntityNotFoundException extends RuntimeException{
-    public EntityNotFoundException(String msg){
-        super(msg);
-    }
-}
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,19 +3,14 @@ package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.DTOs.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
-import com.devsuperior.dscatalog.expections.EntityNotFoundException;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.lang.annotation.Retention;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-
 
 
 @Service
@@ -24,27 +19,11 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
-    @Autowired
-    private ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
-    }
-    @Transactional(readOnly = true)
-    public CategoryDTO findById(Long id) {
-        Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
-        return new CategoryDTO(entity);
-
-    }
-    @Transactional
-    public CategoryDTO insert(CategoryDTO dto) {
-        Category entity = new Category();
-        entity.setName(dto.getName());
-        entity = repository.save(entity);
-        return new CategoryDTO(entity);
     }
 }
