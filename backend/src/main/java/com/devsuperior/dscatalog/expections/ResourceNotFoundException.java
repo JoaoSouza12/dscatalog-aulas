77e1f7570a9471da1430a16c19217042ff0065e8
João <joao.sousa.12145@gmail.com>
2024-12-12 15:44:14 -0300

    Criação das classes de tratamento de erros: ResourceNotFound.

---
+++ backend/src/main/java/com/devsuperior/dscatalog/expections/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscatalog.expections;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+}
