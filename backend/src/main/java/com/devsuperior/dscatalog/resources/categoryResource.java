98aeaceb8cedb4abd90b248c93a776c282aa6510
João <joao.sousa.12145@gmail.com>
2024-12-12 15:01:33 -0300

    Criação do método insert da entidade Category.

--- backend/src/main/java/com/devsuperior/dscatalog/resources/categoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/categoryResource.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,5 +29,12 @@ public class categoryResource {
     public ResponseEntity<List<CategoryDTO>> findAll() {
         List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(CategoryDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.ok().body(dto);
     }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -26,4 +26,11 @@ public class CategoryService {
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
     }
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
 }
