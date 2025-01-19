b8c7da3cb29c7e67a6e536e493f4e9883d88c644
Jose <joao.sousa.12145@gmail.com>
2025-01-19 09:33:00 -0300

    Atualização do projeto.

--- backend/src/com/dscatalog/repositories/ProductRepository.java
+++ backend/src/com/dscatalog/repositories/ProductRepository.java
@@ -3,5 +3,6 @@ package com.dscatalog.repositories;
 import com.dscatalog.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface
+ProductRepository extends JpaRepository<Product, Long> {
 }
